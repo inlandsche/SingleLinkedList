e13bc93cc3024a95da8c2181a5021b9a9e49ba73
inlandsche <79891678+inlandsche@users.noreply.github.com>
2021-03-02 11:43:46 +0700

    Create Node.java

---
+++ Node.java
@@ -0,0 +1,18 @@
+public class Node {
+    Object data;
+    Node pointer;
+
+    Node(){
+
+    }
+
+    Node(Object data){
+        this.data = data;
+    }
+
+    Node(Object data, Node pointer){
+        this.data = data;
+        this.pointer = pointer;
+    }
+}
+
